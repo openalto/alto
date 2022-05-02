0e7e71266629f73cb55a2c85c6b186a80a99d6c5
jacobdunefsky <60287362+jacobdunefsky@users.noreply.github.com>
2022-05-02 01:59:00 -0400

    Move do_request to get_throughput in client.py

--- src/alto/app/estimator.py
+++ src/alto/app/estimator.py
@@ -3,17 +3,12 @@ alto-estimator.py
 
 Estimates the throughput of a number of different flows.
 
-Can be used in two ways:
-
-1. From other Python code. Call do_request(input_dict, alto_server)
-    where input_dict is a Python dict representing the "endpoints-flows"
-    argument, and where alto_server is the hostname of the ALTO server.
-2. As a standalone script. In this case, --alto-server gives the hostname
-    of the ALTO server, and --flows gives a path to an input file.
-    The flows file is of the form
-        SRC1 -> DST1 DST2 DST3 ...
-        SRC2 -> DST4 DST5 DST6 ...
-        ...
+Can be used as a standalone script. --alto-server gives the hostname
+	of the ALTO server, and --flows gives a path to an input file.
+	The flows file is of the form
+		SRC1 -> DST1 DST2 DST3 ...
+		SRC2 -> DST4 DST5 DST6 ...
+		...
 """
 
 import json
@@ -33,7 +28,7 @@ def input_to_json(input_str):
 	Returns:
 		A list of flows.
 	"""
-    input_lines = input_str.splitlines()
+	input_lines = input_str.splitlines()
 	"""
 	TODO: add compression. https://github.com/openalto/alto/issues/7
 	What does "compression" mean in this context? As was decided during a
@@ -60,51 +55,17 @@ def input_to_json(input_str):
 		Thus, the current code simply naively translates the input string into
 		a dict.
 	"""
-    ef_arr = []
-    for line in input_lines:
-        line_split = line.split("->")
-        src = line_split[0].strip()
-        dst_arr = line_split[1].strip().split(" ")
-        dst_arr = list(filter(lambda a: a != 0, dst_arr))
+	ef_arr = []
+	for line in input_lines:
+		line_split = line.split("->")
+		src = line_split[0].strip()
+		dst_arr = line_split[1].strip().split(" ")
+		dst_arr = list(filter(lambda a: a != 0, dst_arr))
 
-        ef_arr.append({"srcs": [src], "dsts": dst_arr})
-    return ef_arr
+		ef_arr.append({"srcs": [src], "dsts": dst_arr})
+	return ef_arr
 
-def do_request(input_dict, alto_server):
-	"""Obtains ALTO throughput data for an input list representing flows
-		of interest.
-
-	Args:
-		input_dict (list): A list of dicts, representing a list of flows.
-			The list should be of the form
-			[{"srcs": [src1, src2, src3, ...], "dsts": [dst1, dst2, dst3, ...]},
-			{"srcs": [src4, src5, src6, ...], "dsts": [dst4, dst5, dst6, ...]},
-			...]
-
-		alto_server (str): The base URL for the ALTO server. This URL cannot
-			end in a "/".
-	Returns:
-		A JSON string representing the throughput for each flow
-	"""
-
-    alto_endpoint = "/endpoint/cost"
-    query_json = {
-        "cost-type": {"cost-mode" : "numerical",
-                      "cost-metric" : "tput"},
-        "endpoint-flows" : input_dict
-    }
-    query_str = json.dumps(query_json)
-    query_headers = {
-        "Content-Type": "application/alto-endpointcostparams+json",
-        "Accept": "application/alto-endpointcost+json,application/alto-error+json"
-    }
-    alto_r = requests.post(alto_server + alto_endpoint,
-        headers=query_headers,
-        data=query_str
-    )
-
-    alto_json_resp = alto_r.json()
-    return json.dumps(alto_json_resp)
+from alto.client import Client
 
 def do_request_from_str(input_str, alto_server):
 	"""Obtains ALTO throughput data for an input string representing flows
@@ -123,7 +84,8 @@ def do_request_from_str(input_str, alto_server):
 	Returns:
 		A JSON string representing the throughput for each flow
 	"""
-    return do_request(input_to_json(input_str), alto_server)
+	c = Client()
+	return c.get_throughput(input_to_json(input_str), url=alto_server+"/endpoint/cost")
 
 if __name__ == "__main__":
 	"""Obtains ALTO throughput data for an input file representing flows
@@ -142,18 +104,18 @@ if __name__ == "__main__":
 	Returns:
 		A JSON string representing the throughput for each flow
 	"""
-    import argparse
-    import sys
+	import argparse
+	import sys
 
-    parser = argparse.ArgumentParser(description="Estimate throughput for flows")
-    parser.add_argument('--alto-server', required=True)
-    parser.add_argument('--flows', required=True)
-    args = parser.parse_args(sys.argv[1:])
+	parser = argparse.ArgumentParser(description="Estimate throughput for flows")
+	parser.add_argument('--alto-server', required=True)
+	parser.add_argument('--flows', required=True)
+	args = parser.parse_args(sys.argv[1:])
 
-    alto_server = args.alto_server
+	alto_server = args.alto_server
 
-    fp = open(args.flows, "r")
-    input_str = fp.read()
-    fp.close()
+	fp = open(args.flows, "r")
+	input_str = fp.read()
+	fp.close()
 
-    print(do_request_from_str(input_str, alto_server))
+	print(do_request_from_str(input_str, alto_server))
